ff1536565a4ad964f91052f9f110dae3b598d72f
NordestGames <anailton.af.af@gmail.com>
2025-09-24 23:41:12 -0300

    Adicionado desafio 1010

---
+++ 1-Iniciante/Main1010.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main1010 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in).useLocale(Locale.US);
+
+        int codPeca1 = leitor.nextInt();
+        int qntPeca1 = leitor.nextInt();
+        double valorPeca1 = leitor.nextDouble();
+
+        int codPeca2 = leitor.nextInt();
+        int qntPeca2 = leitor.nextInt();
+        double valorPeca2 = leitor.nextDouble();
+
+        double valorTotal = (valorPeca1 * qntPeca1) + (valorPeca2 * qntPeca2);
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", valorTotal);
+
+        leitor.close();
+    }
+}
